accb1ee168f20f4650a0202f5493e434fe90a12c
cesarlm05 <cesarlm05@gmail.com>
2022-08-16 22:43:59 -0300

    Add SweetAlert

---
+++ js/sweetalet.js
@@ -0,0 +1,11 @@
+Swal.fire({
+	title: `Welcome to our Shop Phones`,
+    html: `<p>We use our own and third-party cookies to obtain statistical data on our users' browsing and improve our services. If you accept or continue browsing, we consider that you accept its use. You can change the settings or get more information <a>here</a></p>`,
+    icon: `info`,
+    background: `#000`,
+    timer: `7500`,
+    allowOutsideClick: false,
+	allowEscapeKey: false,
+	allowEnterKey: false,
+	stopKeydownPropagation: false
+});
