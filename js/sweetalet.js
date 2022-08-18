37388636b71ecd4b865558185103582e94177e1c
cesarlm05 <cesarlm05@gmail.com>
2022-08-18 19:27:05 -0300

    Update Styles

--- js/sweetalet.js
+++ js/sweetalet.js
@@ -1,6 +1,6 @@
 Swal.fire({
-	title: `Welcome to our Shop Phones`,
-    html: `<p>We use our own and third-party cookies to obtain statistical data on our users' browsing and improve our services. If you accept or continue browsing, we consider that you accept its use. You can change the settings or get more information <a>here</a></p>`,
+	title: `Bienvevidos a VERSATILE`,
+    html: `<p>Utilizamos cookies propias y de terceros para obtener datos estadísticos de la navegación de nuestros usuarios y mejorar nuestros servicios. Si acepta o continúa navegando, consideramos que acepta su uso. Puede cambiar la configuración u obtener más información aquí</p>`,
     icon: `info`,
     background: `#000`,
     timer: `7500`,
