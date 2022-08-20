f4dec952931cc2c687f7fa75de29c8eacabe46aa
cesarlm05 <cesarlm05@gmail.com>
2022-08-20 16:34:07 -0300

    sweetalet

---
+++ js/sweetalet.js
@@ -0,0 +1,21 @@
+/*!
+ * Start Bootstrap - Shop Homepage v5.0.5 (https://startbootstrap.com/template/shop-homepage)
+ * Copyright 2013-2022 Start Bootstrap
+ * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-shop-homepage/blob/master/LICENSE)
+ */
+// This file is intentionally blank
+// Use this file to add JavaScript to your project
+
+// sweetalet
+
+Swal.fire({
+	title: `Bienvevidos a VERSATILE`,
+    html: `<p>Utilizamos cookies propias y de terceros para obtener datos estadísticos de la navegación de nuestros usuarios y mejorar nuestros servicios. Si acepta o continúa navegando, consideramos que acepta su uso. Puede cambiar la configuración u obtener más información aquí</p>`,
+    icon: `info`,
+    background: `#000`,
+    timer: `7500`,
+    allowOutsideClick: false,
+	allowEscapeKey: false,
+	allowEnterKey: false,
+	stopKeydownPropagation: false
+});
